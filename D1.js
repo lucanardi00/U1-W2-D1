d3d225681fa72b052c7c277c70cd04cda9cd9119
luca <lucabean@live.it>
2024-03-05 15:55:18 +0100

    modified js

--- D1.js
+++ D1.js
@@ -74,6 +74,4 @@ let name1 = 'john'
 let name2 = 'John'
 console.log(name1 === name2)
 
-if ((name1 = name2)) {
-  console.log(name1 === name2)
-}
+console.log(name1.toLowerCase === name2.toLowerCase())
